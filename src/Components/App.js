9d5ee79568dc965affa4b5d31741e67d72715e48
tahsandewan <tahsandewan@gmail.com>
2020-05-03 21:08:43 +0600

    footer

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Basic form</h1>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import footer from "./footer.css";
+
+class Footer extends Component {
+  render() {
+    return (
+      <div class="footer-bottom">
+        Copyright © Kakaku.com, Inc. All Rights Reserved. All Rights Reserved.{" "}
+      </div>
+    );
+  }
+}
+
+export default Footer;
